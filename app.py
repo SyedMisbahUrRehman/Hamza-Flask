3515e78cf12264235cea790d133e4cfbf0a0a363
syedmisbahurrehman <syedmisbah588@gmail.com>
2024-02-22 15:06:29 +0500

    print on console

--- app.py
+++ app.py
@@ -14,7 +14,9 @@ def values():
 
 @app.route('/display_values/<val1>/<val2>')
 def display_values(val1, val2):
-    return render_template('pass.html', val1=val1, val2=val2)
+    print("Your passed values are : ", val1, val2)
+    return render_template('pass.html', val1=val1, val2=val2), 200
+
 
 if __name__ == '__main__':
     app.run(debug=True)
